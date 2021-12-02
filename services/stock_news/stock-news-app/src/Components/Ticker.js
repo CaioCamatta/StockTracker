7245b006b7d71e6a11f5de4381a40965f7cac29b
Daniel Dragon Wakefield <dwakefie@uwo.ca>
2021-12-02 17:15:24 -0500

    Updated frontend

--- services/stock_news/stock-news-app/src/Components/Ticker.js
+++ services/stock_news/stock-news-app/src/Components/Ticker.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Form } from "react-bootstrap";
 
 const Ticker = ({getTick}) => {
     const [ticker, setTick] = useState("");
-
+  
     const sendTicker = (event) =>{
         event.preventDefault();
-        console.log("Send: ", ticker);
+
         getTick(ticker);
     }
-    console.log("Not send:" , ticker);
+    
     return(
         <div className="mb-5">
-            <form onSubmit = {sendTicker}>
-            <input style={{padding: "0.5% 15% 0.5% 0%", marginTop: "1.5em", borderColor: "gray"}} type="text" value={ticker} onInput = {e => setTick(e.target.value)} placeholder="Enter Stock Ticker"/>
-            </form>
+            <Form onSubmit = {sendTicker}>
+            <Form.Control style={{width: "40%"}} type="text" value={ticker} onInput = {e => setTick(e.target.value)} placeholder="Enter Stock Ticker"/>
+            </Form>
             
         </div>
     );
