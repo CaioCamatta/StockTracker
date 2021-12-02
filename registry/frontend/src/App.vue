508170ffba71bdfefd128367ae203586c76b0b48
Caio Coelho <caioz.co@gmail.com>
2021-12-02 13:52:55 -0500

    Fix paths not working, styling, add footer

--- registry/frontend/src/App.vue
+++ registry/frontend/src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <div id="app">
-    <b-navbar toggleable="lg" type="dark" variant="primary" id="#navbar">
+  <div id="app" class="position-relative">
+    <b-navbar toggleable="lg" type="dark" id="#navbar" class="bg-purple">
       <div class="container">
         <b-navbar-brand href="/">StockTracker</b-navbar-brand>
       </div>
     </b-navbar>
-    <div class="container">
+    <div class="container pb-5">
       <HelloWorld :servicesData="servicesData" />
+    </div>
+    <div class="pt-5">
+      <footer class="bg-light-purple container-fluid">
+        <div class="container py-3">
+          Made by Caio C. Coelho, Caleb Sutherland, Daniel Wakefield, Jack
+          Hillier, Ryan Harlen.
+          <a href="https://github.com/CaioCamatta/StockTracker">Github</a>
+        </div>
+      </footer>
     </div>
   </div>
 </template>
@@ -25,9 +34,12 @@ export default {
     };
   },
   async created() {
-    const res = await fetch("http://localhost:3000/services", {
-      method: "GET",
-    }).then((response) => response.json());
+    const res = await fetch(
+      process.env.VUE_APP_REGISTRY_BACKEND_URL + "services",
+      {
+        method: "GET",
+      }
+    ).then((response) => response.json());
     this.servicesData = res;
   },
 };
@@ -39,5 +51,16 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  min-height: 100vh;
+}
+.bg-purple {
+  background-color: #7749f8 !important;
+}
+.bg-light-purple {
+  background-color: #ebe5fc;
+}
+footer {
+  position: absolute;
+  bottom: 0;
 }
 </style>
