222dd0100ab65c5f9244add9001a51031903cb3a
RyanH35 <rharlen35@gmail.com>
2021-12-02 20:09:15 -0500

    Visual Updates
    Added visual updates and extra information above graph

--- services/stock_graph/frontend/src/components/StockData.js
+++ services/stock_graph/frontend/src/components/StockData.js
@@ -7,45 +7,57 @@ class StockData extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            stockData: [ ]
+            stockData: [ ],
+            currentPrice: "",
+            growth: "",
+            mean: 0,
+            deviationSum: 0
+
         }
     }
-
     componentDidMount() {
-        //query marketStack API
-        const url = `${marketStack.base_url}/eod?access_key=${marketStack.api_token}&symbols=${this.props.ticker}`
-        
-        //add data to graph
-        fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            for (var i =0; i < this.props.dataLength; i++)
+            for (var i = 0; i >= 0; i--)
             {
                 this.setState({
-                stockData: this.state.stockData.concat({ date: data['data'][i]['date'].split("T")[0], price: data['data'][i]['close']})
+                stockData: this.state.stockData.concat({ date: "N/A", price: 0})
                 })
 
             }
-        })
     }
 
     componentDidUpdate(prevProps, prevState) {
         //query marketStack API
         const url = `${marketStack.base_url}/eod?access_key=${marketStack.api_token}&symbols=${this.props.ticker}`
         
-        if(prevProps.ticker !== this.props.ticker || prevProps.dataLength !== this.props.dataLength){
+        if((prevProps.ticker !== this.props.ticker || prevProps.dataLength !== this.props.dataLength) && this.props.ticker != ""){
             //add data to graph
             fetch(url)
             .then((response) => response.json())
             .then((data) => {
                 this.state.stockData = [];
-                for (var i =0; i < this.props.dataLength; i++)
+                this.setState({currentPrice: data['data'][0]['close']})
+                this.setState({growth: 
+                    (((data['data'][0]['close']-data['data'][this.props.dataLength-1]['close'])/
+                    data['data'][this.props.dataLength-1]['close'])*100).toFixed(2) 
+                
+                })
+                this.state.mean = 0
+                this.state.deviationSum = 0
+                for (var i = this.props.dataLength-1; i >= 0; i--)
                 {
                     this.setState({
                     stockData: this.state.stockData.concat({ date: data['data'][i]['date'].split("T")[0], price: data['data'][i]['close']})
                     })
+                    this.state.mean = this.state.mean + data['data'][i]['close']
 
                 }
+                this.state.mean = this.state.mean/this.props.dataLength;
+                for (var i = this.props.dataLength-1; i >= 0; i--)
+                {
+                    this.state.deviationSum = this.state.deviationSum + ((data['data'][i]['close']-this.state.mean)**2)
+                }
+                this.state.deviationSum = this.state.deviationSum/this.props.dataLength
+                this.setState({deviationSum: Math.sqrt(this.state.deviationSum).toFixed(2)})
             })
         }
         
@@ -56,6 +68,9 @@ class StockData extends Component {
         return (
             <div>
                 <h2>{this.props.ticker}</h2>
+                Last EOD Price: ${this.state.currentPrice}<br />
+                Growth: {this.state.growth}%<br />
+                Volatility: {this.state.deviationSum}
                 <AreaChart
                     width={1500}
                     height={550}
@@ -73,6 +88,9 @@ class StockData extends Component {
                         fill='#8884d8'
                         />
                 </AreaChart>
+                <br />
+                <br />
+                <br />
 
             </div> 
         ) 
--- services/stock_graph/frontend/src/config/marketStack.js
+++ services/stock_graph/frontend/src/config/marketStack.js
@@ -1,6 +1,7 @@
 export const marketStack = {
     //9660e192ea7dfa64eb6c627e8d78431f
     //fb48713a74401dbfa61145b20ff3b6b5
-    api_token: 'b20f8a34cd7edd1b856b3449c50ed00a', 
+    //b20f8a34cd7edd1b856b3449c50ed00a
+    api_token: '9660e192ea7dfa64eb6c627e8d78431f', 
     base_url: 'http://api.marketstack.com/v1'
 };
